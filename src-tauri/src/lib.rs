6a2837c1cada051c86e6bc767f0062718e89e2bf
Tanner Peterson <tannermp23@hotmail.com>
2024-10-14 15:37:55 -0500

    remove logos from UI; use vertex + index buffers

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,8 @@
-use std::{
-    borrow::Cow,
-    ops::Range,
-    sync::Mutex,
-    time::{Duration, SystemTime},
-};
+use std::{sync::Mutex, time::Duration};
 
 use tauri::{async_runtime::block_on, AppHandle, Manager, PhysicalSize, RunEvent, WindowEvent};
 use tokio::time::sleep;
+use wgpu::{include_wgsl, util::DeviceExt as _};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -14,44 +10,86 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [-0.45, 0.9, 0.0],
+        color: [0.5, 0.5, 0.5],
+    },
+    Vertex {
+        position: [-0.45, 0.0, 0.0],
+        color: [0.3, 0.3, 0.3],
+    },
+    Vertex {
+        position: [0.45, 0.0, 0.0],
+        color: [0.5, 0.5, 0.5],
+    },
+    Vertex {
+        position: [0.45, 0.9, 0.0],
+        color: [0.7, 0.7, 0.7],
+    },
+];
+
+const INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
+
+struct GpuState<'a> {
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    render_pipeline: wgpu::RenderPipeline,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+}
+
 // TODO
 // 1) don't use a new webview
-//  - create components around the video player that are do not have transparent backgrounds
-//  - figure out how to only draw to the section where the video player should be
-//  - make sure things can be rendered on top of it
-//  - make sure resizing works
+//  - create components around the video player that do not have transparent backgrounds
+//
+// IDEAS:
+//  - add text input boxes to set min/max threshold and change to blue/red if pixels ar
+//    darker/brighter than the threshold. Might need to make the image greyscale to do this
+//  - Set a default texture when video is not playing
+//  - Vary the image over time, or find some other images to shuffle through
+//  ? make some resizable component in the FE, send the size and position down to rust, have that
+//    control where the video is rendered in the shader
 
 fn next_triangle(app_handle: &AppHandle, new_size: Option<PhysicalSize<u32>>) {
-    let surface = app_handle.state::<Mutex<wgpu::Surface>>();
-    let render_pipeline = app_handle.state::<wgpu::RenderPipeline>();
-    let device = app_handle.state::<wgpu::Device>();
-    let queue = app_handle.state::<wgpu::Queue>();
+    let gpu_state_mutex = app_handle.state::<Mutex<GpuState>>();
 
     {
-        let surface_ = surface.lock().unwrap();
+        let mut gpu_state = gpu_state_mutex.lock().unwrap();
         if let Some(new_size) = new_size {
-            let config = app_handle.state::<Mutex<wgpu::SurfaceConfiguration>>();
-            let mut config = config.lock().unwrap();
-            config.width = if new_size.width > 0 {
+            gpu_state.config.width = if new_size.width > 0 {
                 new_size.width
             } else {
                 1
             };
-            config.height = if new_size.height > 0 {
+            gpu_state.config.height = if new_size.height > 0 {
                 new_size.height
             } else {
                 1
             };
-            surface_.configure(&device, &config);
+            gpu_state
+                .surface
+                .configure(&gpu_state.device, &gpu_state.config);
         }
-        let frame = surface_
+        let frame = gpu_state
+            .surface
             .get_current_texture()
             .expect("Failed to acquire next swap chain texture");
         let view = frame
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
-        let mut encoder =
-            device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+        let mut encoder = gpu_state
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
         {
             let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: None,
@@ -67,20 +105,24 @@ fn next_triangle(app_handle: &AppHandle, new_size: Option<PhysicalSize<u32>>) {
                 timestamp_writes: None,
                 occlusion_query_set: None,
             });
-            rpass.set_pipeline(&render_pipeline);
-            let now = (SystemTime::now()
-                .duration_since(SystemTime::UNIX_EPOCH)
-                .expect("failed getting dur since")
-                .as_secs()
-                % 4) as u32;
-            let r = Range {
-                start: now,
-                end: now + 3,
-            };
-            rpass.draw(r, 0..1);
+            rpass.set_pipeline(&gpu_state.render_pipeline);
+            // let now = (SystemTime::now()
+            //     .duration_since(SystemTime::UNIX_EPOCH)
+            //     .expect("failed getting dur since")
+            //     .as_secs()
+            //     % 4) as u32;
+            // let r = Range {
+            //     start: now,
+            //     end: now + 3,
+            // };
+            // rpass.draw(r, 0..1);
+
+            rpass.set_vertex_buffer(0, gpu_state.vertex_buffer.slice(..));
+            rpass.set_index_buffer(gpu_state.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            rpass.draw_indexed(0..INDICES.len() as u32, 0, 0..1);
         }
 
-        queue.submit(Some(encoder.finish()));
+        gpu_state.queue.submit(Some(encoder.finish()));
         frame.present();
     }
 }
@@ -112,39 +154,48 @@ pub fn run() {
             .expect("Failed to find an appropriate adapter");
 
             // Create the logical device and command queue
-            let (device, queue) = block_on(
-                adapter.request_device(
-                    &wgpu::DeviceDescriptor {
-                        label: None,
-                        required_features: wgpu::Features::empty(),
-                        memory_hints: wgpu::MemoryHints::Performance,
-                        // Make sure we use the texture resolution limits from the adapter, so we can support images the size of the swapchain.
-                        required_limits: wgpu::Limits::downlevel_webgl2_defaults()
-                            .using_resolution(adapter.limits()),
-                    },
-                    None,
-                ),
-            )
+            let (device, queue) = block_on(adapter.request_device(
+                &wgpu::DeviceDescriptor {
+                    label: None,
+                    required_features: wgpu::Features::empty(),
+                    memory_hints: wgpu::MemoryHints::Performance,
+                    // Make sure we use the texture resolution limits from the adapter, so we can support images the size of the swapchain.
+                    required_limits: wgpu::Limits::default().using_resolution(adapter.limits()),
+                },
+                None,
+            ))
             .expect("Failed to create device");
 
             // Load the shaders from disk
-            let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-                label: None,
-                source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(
-                    r#"
-@vertex
-fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
-    let x = f32(i32(in_vertex_index % 3) - 1) / 1.8;
-    let y = f32(i32(in_vertex_index & 1u) * 2 - 1) / 1.8 + (1.0 / 3.0);
-    return vec4<f32>(x, y, 0.0, 1.0);
-}
+            let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));
 
-@fragment
-fn fs_main() -> @location(0) vec4<f32> {
-    return vec4<f32>(0.2, 0.0, 1.0, 1.0);
-}
-"#,
-                )),
+            let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            });
+
+            let vertex_buffer_layout = wgpu::VertexBufferLayout {
+                array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+                step_mode: wgpu::VertexStepMode::Vertex,
+                attributes: &[
+                    wgpu::VertexAttribute {
+                        offset: 0,
+                        shader_location: 0,
+                        format: wgpu::VertexFormat::Float32x3,
+                    },
+                    wgpu::VertexAttribute {
+                        offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                        shader_location: 1,
+                        format: wgpu::VertexFormat::Float32x3,
+                    },
+                ],
+            };
+
+            let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
             });
 
             let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
@@ -163,7 +214,7 @@ fn fs_main() -> @location(0) vec4<f32> {
                     module: &shader,
                     entry_point: "vs_main",
                     compilation_options: wgpu::PipelineCompilationOptions::default(),
-                    buffers: &[],
+                    buffers: &[vertex_buffer_layout],
                 },
                 fragment: Some(wgpu::FragmentState {
                     module: &shader,
@@ -214,18 +265,27 @@ fn fs_main() -> @location(0) vec4<f32> {
                     timestamp_writes: None,
                     occlusion_query_set: None,
                 });
+
                 rpass.set_pipeline(&render_pipeline);
-                rpass.draw(0..1, 0..1);
+                rpass.set_vertex_buffer(0, vertex_buffer.slice(..));
+                rpass.set_index_buffer(index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+                rpass.draw_indexed(0..1, 0, 0..1);
             }
 
             queue.submit(Some(encoder.finish()));
             frame.present();
 
-            app.manage(Mutex::new(surface));
-            app.manage(render_pipeline);
-            app.manage(device);
-            app.manage(queue);
-            app.manage(Mutex::new(config));
+            let gpu_state = GpuState {
+                surface,
+                device,
+                queue,
+                config,
+                render_pipeline,
+                vertex_buffer,
+                index_buffer,
+            };
+
+            app.manage(Mutex::new(gpu_state));
 
             Ok(())
         })
--- src/App.jsx
+++ src/App.jsx
@@ -18,22 +18,11 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="row" style={{height: "300px"}}>
       </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
+      <h1>Welcome to Tauri!</h1>
       <form
         className="row"
         onSubmit={(e) => {
